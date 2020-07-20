2891d705f7e5f6bae3f7c3dc4c4bbb0b9de60dad
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2020-07-20 14:49:29 +0100

    chore: export Middleware type

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { createStore } from './createStore';
 export { my } from './helpers/my';
 export { freeze } from './middleware/freeze';
 export { logger } from './middleware/logger';
+export type { Middleware } from './middleware/middleware.type';
 export { persist } from './middleware/persist';
 export { createStoreContext } from './react/createStoreContext';
